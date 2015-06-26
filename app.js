e491436f235995d96b7aa778eabe6698e3c85a7c
Alan Campbell <alan.campbell@castawaylabs.com>
2015-06-26 12:55:46 +0100

    functionless

--- app.js
+++ app.js
@@ -5,32 +5,28 @@ var koa		= require('koa'),
 	users	= wrap(db.get('users2')),
 	app		= koa();
 
-function* findDupes() {
-	var queue, user, x, y, completed, partials, key;
-	
-	console.log('Finding Dupes...');
-	queue = yield users.find({username: ''});
+var queue, user, x, y, completed, partials, key;
 
-	for (x = 0; x < queue.length; x++) {
-		user = {};
-		partials = yield users.find({phone: queue[x].phone});
+console.log('Finding Dupes...');
+queue = yield users.find({username: ''});
 
-		console.log(queue[x].phone);
+for (x = 0; x < queue.length; x++) {
+	user = {};
+	partials = yield users.find({phone: queue[x].phone});
 
-		for (y = 0; y < partials.length; y++) {
-			for (key in partials[y]) {
-				if (partials[y].key.length > 0) {
-					user.key = partials[y].key;
-				}
+	console.log(queue[x].phone);
+
+	for (y = 0; y < partials.length; y++) {
+		for (key in partials[y]) {
+			if (partials[y].key.length > 0) {
+				user.key = partials[y].key;
 			}
-			yield users.remove(partials[y]);
 		}
-
-		yield users.remove(queue[x]);
-
-		yield users.insert(user);
-		completed = queue[x].phone;
+		yield users.remove(partials[y]);
 	}
-}
 
-findDupes();
+	yield users.remove(queue[x]);
+
+	yield users.insert(user);
+	completed = queue[x].phone;
+}
